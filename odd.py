c3fa93ce4c2635cea11b8c8142d9d8454ca34651
HARSHITAIENGINEER <165182815+HARSHITAIENGINEER@users.noreply.github.com>
2024-03-27 20:58:23 +0530

    Create odd.py

---
+++ odd.py
@@ -0,0 +1,22 @@
+class PrimeChecker:
+    def is_prime(self, n):
+        if n <= 1:
+            return False
+        if n <= 3:
+            return True
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+# Example usage:
+prime_checker = PrimeChecker()
+number = int(input("Enter a number: "))
+if prime_checker.is_prime(number):
+    print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
